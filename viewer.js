043e4fe7191149c4be1ddc5b538c8d75c43712ba
Niall Robinson <niall.h.robinson@gmail.com>
2015-02-16 13:37:18 +0000

    demonstration of ploblem

--- viewer.js
+++ viewer.js
@@ -16,7 +16,7 @@ function initVis() {
 
     /***************** Data Cloud **********************/
     // load texture
-    dataTexture = THREE.ImageUtils.loadTexture('./test_blob2.png');
+    dataTexture = THREE.ImageUtils.loadTexture('./test_cube.png');
 
     var boxGeometry = new THREE.BoxGeometry(1.0, 1.0, 1.0); // the block to render inside
     boxGeometry.doubleSided = true;
