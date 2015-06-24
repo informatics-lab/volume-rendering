60029718592060bfb741064b240fd99c53971dab
Rachel Prudden <rachel@Admins-MacBook-Pro-5.local>
2015-06-24 16:31:53 +0100

    Got rid of stripes bug.

--- viewer.js
+++ viewer.js
@@ -160,12 +160,10 @@ function initVis() {
     //file = "out_623_812_59_4096_4096.webm";
     //file = "datanshadows_623_812_59_4096_4096.ogv"
     //file = "out_251_325_34_256_4096.ogv";
-    var url = "http://ec2-52-16-246-202.eu-west-1.compute.amazonaws.com:9000/molab-3dwx-ds/media/55896829e4b0b14cba17273c";
+    //var url = "http://ec2-52-16-246-202.eu-west-1.compute.amazonaws.com:9000/molab-3dwx-ds/media/55896829e4b0b14cba17273c";
+    var url = "http://ec2-52-16-246-202.eu-west-1.compute.amazonaws.com:9000/molab-3dwx-ds/media/5589758be4b0b14cba172762";
     var file = url + "/data";
     var dims = getDims(url);
-    //dims = getDimensions("out_251_327_34_256_4096.ogv");
-    // file = "http://ec2-52-16-246-202.eu-west-1.compute.amazonaws.com:9000/molab-3dwx-ds/media/5589758be4b0b14cba172762/data";
-    // dims = getDimensions("out_345_449_54_512_8192.ogv");
 
     var ds = dims.datashape;
     var boxDims = new THREE.Vector3(ds.x*0.001, ds.z*0.01, ds.y*0.001);
